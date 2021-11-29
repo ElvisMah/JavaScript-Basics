2ee46e82def9c8b5984f268ca1ee977ad7e6e360
elvis.mahmutovic@oasisdigital.com <elvis.mahmutovic@oasisdigital.com>
2021-11-29 12:28:00 -0700

    added first hour notes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # JavaScript-Basics
 
 ## First Hour
+
+### Inline JS, Internal JS, External
+
+### Helper Methods
+
+### Statements and Comments
---
+++ main.js
@@ -0,0 +1,16 @@
+ document.querySelectorAll('.btn').forEach((item) => {
+        item.addEventListener('click', () => {
+          alert('Hello Caps!!!!!!');
+        });
+      });
+
+// Helper methods
+
+alert({name: 'Elvis',
+       email: 'elvis@elvis.com'});
+
+document.write({name: 'Elvis',
+       email: 'elvis@elvis.com'});
+
+console.log({name: 'Elvis',
+       email: 'elvis@elvis.com'});
