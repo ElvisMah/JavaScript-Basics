fbeee1bc6998d052136612493048cf713a56e2b1
elvis.mahmutovic@oasisdigital.com <elvis.mahmutovic@oasisdigital.com>
2021-12-03 07:58:20 -0700

    Second Hour

---
+++ 2nd hour/main.js
@@ -0,0 +1,53 @@
+//declare Variable
+
+
+
+
+// let btnDanger = true;
+// const age = 26;
+
+// age1 = 15;
+
+// let lastName;
+
+// lastName = "mahmutovic";
+
+
+
+// const skill = "Elvis's skills are the best";
+
+// const skill2 = 'Elvis\'s skills are the best from skill 2';
+
+// console.log(skill2);
+
+// const firstName = "Elvis";
+// const lastName = "Mahmutovic";
+
+// let fullName;
+
+// fullName = firstName + " " + lastName;
+
+// console.log('Hi, my full name is ' + fullName);
+// console.log(`Hi, my full name is ${fullName}`);
+
+// const website = 'expium';
+
+// const url = 'http://www.' + website + '.com';
+
+// console.log(url);
+
+// let a = 5;
+// let b = 10;
+
+// age = 34;
+
+// console.log(`Fifteen is ${age}`);
+
+let fname = 'elvis'; //string
+let age = 24; //number
+let age3 = "46"; 
+let userProfile = true ; //boolean
+
+console.log(fname);
+console.log(age);
+console.log(age3);
